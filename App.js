d7c49fee768b2e3f0d244fa72b27ab66585a9821
MD-EHTASHAMUDDIN <150144250+MD-EHTASHAMUDDIN@users.noreply.github.com>
2024-01-16 09:05:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './user';
+
+function App() {
+ return(
+    <div className="App">
+    
+        <h1>Hello World !</h1>
+        <User/>
+    </div>
+  );
+}
+
+export default App;
